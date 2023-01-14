11529faf8c9d9049a68f8bbdbadc856cbc4d96d3
KimJacobus <jacobus.kim@gmail.com>
2023-01-15 00:29:54 +0100

    animation skull, onclick

--- animation.js
+++ animation.js
@@ -1,2 +1,27 @@
-var crane = document.querySelectorAll('#crane'); 
+var skull = document.getElementById('skull'); 
 
+var click = document.querySelector('#click');
+
+var block = true;
+
+click.addEventListener("click", SkullAppear);
+
+function SkullAppear(){ 
+    
+    if(block) {
+        
+        
+        block = false;
+        
+        skull.classList.remove("hidden");
+        
+        
+  setTimeout(()=> {
+
+      block = true; 
+
+    skull.classList.add("hidden");
+  },2000)
+
+}
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,22 +12,19 @@ module.exports = {
     extend: {
 
       animation: { 
-        'skull': 'skull 1s infinite', 
-
+        'skull': 'skull 2s', 
         'waving-hand': 'wave 2s linear infinite',
         'fading': 'fade 4s forwards',
       },
 
       keyframes: {
       skull: {
-        '0%, 100%' : { transform: 'translateY(100%)', opacity: '0%' },
-        '25%' : {opacity : '75%'},
-        '50%': { transform: 'translateY(0%)', opacity: '100%' },
-        '75%': {opacity : '75%'},
+        '0%' : { transform: 'translateY(500%)', opacity: '0%' },
+        '50%': { opacity: '100%' },
+        '100%': { transform: 'translateY(0%)', opacity : '0%'},
 
         },
-      },
-      keyframes: {
+      
         fade: {
           '0%': { opacity: '1' },
           '100%': { opacity: '0' },
